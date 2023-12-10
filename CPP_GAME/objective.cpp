908aed47440992aac292d057035d6253d7af3c8d
Mabroor Ahmed <109113298+MabroorA@users.noreply.github.com>
2023-12-10 22:31:52 +0000

    First commit

---
+++ CPP_GAME/objective.cpp
@@ -0,0 +1,10 @@
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class Objective {
+public:
+    string type;
+    vector<string> what;
+};
---
+++ CPP_GAME/objects.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Object {
+public:
+
+    string id;
+    string desc;
+    string initialRoom;
+};
